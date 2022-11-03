ee79559b8bd58a8cc46a45339d4fa97f9dcc8d94
Maltewj <maltewj@gmail.com>
2022-11-03 11:05:42 +0100

    Login side JS

---
+++ login.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded",load)
+
+function load(){
+    console.log("JS VIRKER!");
+}
+
+let loginData = [
+    {
+        brugernavn: "abc12345"
+        kodeord: "kodeord1"
+    },
+    {
+        brugernavn: "def6789"
+        kodeord: "kodeord2"
+    }
+];
+
+const usernameInput = document.querySelector("#username-input").value
+const passwordInput = document.querySelector("#password-input").value
+
+document.querySelector("#login-button").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let login = false;
+
+    for(let i = 0; i < loginData.length; i++){
+        if (loginData.brugernavn == usernameInput && loginData.kodeord == passwordInput){
+            login = true;
+            break;
+        } else {
+            document.querySelector("#invalid-input").textContent = "Brugernavn eller kodeord er forkert";
+        }
+    }
+}
