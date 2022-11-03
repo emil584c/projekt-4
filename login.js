c2d94a87add9e8d1de26113ee10133e0111bdcee
VickySonne <vst1996@live.dk>
2022-11-03 12:36:53 +0100

    login fix

--- login.js
+++ login.js
@@ -9,16 +9,18 @@ let loginData = [
     {"brugernavn":"def6789","kodeord":"kodeord2"}
 ];
 
-const usernameInput = document.querySelector("#username-input").value
-const passwordInput = document.querySelector("#password-input").value
-
 document.querySelector("#login-button").addEventListener("click", (e) => {
     e.preventDefault();
+
+    const usernameInput = document.querySelector("#username-input").value;
+    const passwordInput = document.querySelector("#password-input").value;
 
     let login = false;
 
     for(let i = 0; i < loginData.length; i++){
-        if (loginData.brugernavn == usernameInput && loginData.kodeord == passwordInput){
+        console.log(usernameInput);
+        
+        if (loginData[i].brugernavn == usernameInput && loginData[i].kodeord == passwordInput){
             login = true;
             break;
         } else {
