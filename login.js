1c5c58d1b0d224d3cad395b440a705087ec791e8
VickySonne <vst1996@live.dk>
2022-11-04 21:20:14 +0100

    login js opdateres med noter

--- login.js
+++ login.js
@@ -1,31 +1,36 @@
 window.addEventListener("DOMContentLoaded",load);
-function load(){
-    console.log("JS VIRKER!");
-}
 
 // Json Javascript object notation --> Et array med objekter i.
-let loginData = [
+// 
+const loginData = [
     {"brugernavn":"abc12345","kodeord":"kodeord1"},
     {"brugernavn":"def6789","kodeord":"kodeord2"}
 ];
 
-// Eventlistener som reagerer på at man klikker på "login" knappen.
-document.querySelector("#login-button").addEventListener("click", (e) => {
+function load(){
+    console.log("JS VIRKER!");
+
+    // Eventlistener som reagerer på at man klikker på "login" knappen. (e) for det her event.
+    // => arrowfunction (kan få lov at lave en function uden et navn, men den kan ikke kaldes senere, den virker kun lige her)
+    // PreventDefault() fordi der er en default på typen submit som vi gerne vil override.
+    document.querySelector("#login-button").addEventListener("click", (e) => {
     e.preventDefault();
 
-// Variabler for at ungå at skrive det i funktionen.
+    // Variabler for at ungå at skrive det i funktionen.
     const usernameInput = document.querySelector("#username-input").value;
     const passwordInput = document.querySelector("#password-input").value;
 
-    let login = false;
-
     for(let i = 0; i < loginData.length; i++){        
         if (loginData[i].brugernavn == usernameInput && loginData[i].kodeord == passwordInput){
-            login = true;
             window.location = "index.html";
-            break; // Stopper funktionen hvis if-statement er true
+            break; // Stopper loopet hvis if-statement er true og den når til break (kan også bruges til andre ting)
         } else {
             document.querySelector("#invalid-input").innerHTML = "Brugernavn eller kodeord er forkert"; // Targeter en paragraf med invalid-input id, og replacer det med teksten.
         }
     }
 });
+};
+
+// ingen kode der bare "hænger" uden at være i en function, det er generelt ikke godt. man kan ikke styrre hvornår det bliver kørt.
+// den må/kan ikke queryselect hvis dom ikke er loaded, derfor er det vigtigt at eventlistenern er inde i load functionen.
+
