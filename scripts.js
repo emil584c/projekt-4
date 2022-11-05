841cae391f385cfb919c3184d8688e7d691d9d24
emil584c <105641442+emil584c@users.noreply.github.com>
2022-11-05 21:32:32 +0100

    Tilmeld afmeld knap javascript VIRKER!!!

--- scripts.js
+++ scripts.js
@@ -11,37 +11,6 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 
-// filteret
-
-
-// let indicator = document.querySelector('.indicator').children;
-// let main = document.querySelector('.items').children;
-
-// for (let i = 0; i < indicator.length; i++)
-// {
-//   indicator [i].onclick = function(){
-//     for(let x=0; x < indicator.length; x++){
-//       indicator[x].classList.remove('active');
-//     }
-    
-//     this.indicator[x].classList.add('active');
-//     const displayItems = this.getAttribute('data-filter');
-
-
-//     for (let z = 0; x < main.length; z++){
-//       main [z].style.transform = 'scale(0)';
-//       setTimeout(() => {
-//         main[z].style.display = 'none'}, 500);
-
-//         if ((main[z].getAttribute('data-category') == displayItems || displayItems == 'all')){
-
-//             main [z].style.transform = 'scale(0)';
-//         setTimeout(() => {
-//           main[z].style.display = 'block'}, 500);
-//         }
-//     }
-//   }
-// }
 
 
 // hamburger menu
@@ -56,21 +25,27 @@ hamburger.addEventListener("click", () =>{
 
 
 
-
-const statuss = ['Tilmeld', 'Afmeld'];
-const tilmeldKnap = document.querySelectorAll(".zipzap");
+// tilmeld afmeld
+const tilmeldKnap = document.querySelectorAll(".tilmeld-afmeld");
 
 let values = Array.prototype.map.call(tilmeldKnap, function(el) {
-    return el.textContent;
+    return el;
 });
 console.log(values);
 
-let body = document.body
 
-body.addEventListener('load', () => {
-    for (let i = 0; i < values.length; i++) {
-        values[i].addEventListener("click", function() {
-            values[i].textContent = statuss[1];
-        });
-      } 
-});
+
+
+
+values.forEach(buttonText => {
+    buttonText.addEventListener('click', function handleClick(event) {
+        console.log('box clicked', event);
+      if (this.innerHTML == "Tilmeld"){
+        this.innerHTML = "Afmeld";
+      }
+      else{
+        this.innerHTML = "Tilmeld"
+      }
+        
+     });
+    });
