9e46ac6ccc358f83e114c6d761737c8f464b5efc
VickySonne <vst1996@live.dk>
2022-11-05 17:29:36 +0100

    opdateret header-blurry js

--- header-blurry.js
+++ header-blurry.js
@@ -1,22 +1,35 @@
+window.addEventListener("DOMContentLoaded",load);
+
 //Registrere hvor mange pixels der bliver scrollet
-let scrollpos = window.scrollY
+let scrollposition = window.scrollY
 
 const header = document.querySelector("#header")
 
 //definere hvornår headeren skal skifte style
 const scrollChange = 15
 
-//variabler med functioner der tilføjer og fjerner class="header-glass"
-const addClass = () => header.classList.add("header-glass")
-const removeClass = () => header.classList.remove("header-glass")
-
-//eventlistener der lytter efter om scroll finder sted og hvor meget
-window.addEventListener('scroll', function() { 
-  scrollpos = window.scrollY;
-
-//Ved scroll mere en eller ligmed scroll positionen tilføjes class ellers fjernes den
-  if (scrollpos >= scrollChange) { addClass() 
+//Functioner der tilføjer og fjerner class="header-glass"
+function addClass(element){
+  element.classList.add("header-glass");
 }
-  else { removeClass(); }
+
+function removeClass(element){
+  element.classList.remove("header-glass");
 }
-)
+
+function load (){
+  console.log("js linket")
+
+  //eventlistener der lytter efter om scroll finder sted og hvor meget
+  window.addEventListener('scroll', (e) => { 
+    scrollposition = window.scrollY;
+
+    //Ved scroll mere en eller ligmed scroll positionen tilføjes class ellers fjernes den
+    if (scrollposition >= scrollChange) { 
+      addClass(header) 
+    } 
+    else { 
+      removeClass(header) 
+    }
+  });
+};
